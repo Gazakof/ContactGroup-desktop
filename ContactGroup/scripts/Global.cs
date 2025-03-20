1844eb338dec295df9ea6c59bf3bc8d84525cab6
Gazakof <randriamorasata08@gmail.com>
2025-03-20 20:02:00 +0100

    Group srcipt added

---
+++ ContactGroup/scripts/Global.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ContactGroup
+{
+    public static class Global
+    {
+        public static List<Group> contactsGroup = new List<Group>()
+        {
+            new Group ("Friends"),
+            new Group ("Work"),
+            new Group ("Family")
+        };
+    }
+}
--- ContactGroup/scripts/Group.cs
+++ ContactGroup/scripts/Group.cs
@@ -10,9 +10,23 @@ namespace ContactGroup
 
         public string Name { get => name; private set => name = value; }
         public List<Contact> Contacts { get => contactsList; private set => contactsList = value; }
-
         public Group() { }
 
-        public Group(string name) { }
+        public Group(string groupName)
+        {
+            this.Name = groupName;
+            this.Contacts = new List<Contact>();
+        }
+
+        public Group(string groupName, List<Contact> contacts) 
+        {
+            this.Name = groupName;
+            this.Contacts = contacts;
+        }
+
+        public override string ToString()
+        {
+            return this.Name;
+        }
     }
 }
